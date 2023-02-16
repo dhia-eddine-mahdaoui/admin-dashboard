37f1c55381ccd02d0a04adf0744a9e2382ad997d
dhia-eddine-mahdaoui <125560732+dhia-eddine-mahdaoui@users.noreply.github.com>
2023-02-16 20:44:56 +0100

    Add files via upload

---
+++ html/orders.js
@@ -0,0 +1,35 @@
+const Orders =[
+
+
+ {
+productName: 'Galaxy s22 Ultra',
+productNumber: '12345678',
+payment: 'Due',
+status: 'Declined'
+
+},
+{
+productName: 'Iphone 12 Pro',
+productNumber: '958418',
+payment: 'Due',
+status: 'Pending'
+
+},
+{
+    productName: 'Huawei p40 Pro',
+    productNumber: '4522168',
+    payment: 'Due',
+    status: 'Delivered'
+    
+    },
+    {
+        productName: 'OPPO Reno 5',
+        productNumber: '7895132',
+        payment: 'Due',
+        status: 'Declined'
+        
+        }
+        
+    
+
+]
